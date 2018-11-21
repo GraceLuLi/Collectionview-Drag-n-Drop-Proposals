36bdb08f8c2c0744c6af7a2af15bc2ab4491d87b
Grace Lu Li <lu.li.azure@gmail.com>
2018-11-20 23:59:15 -0800

    Create a collectionview app for drag_n_drop.

---
+++ objective-c_dragndrop/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  objective-c_dragndrop
+//
+//  Created by Lu Li on 11/20/18.
+//  Copyright © 2018 GraceLuLi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
